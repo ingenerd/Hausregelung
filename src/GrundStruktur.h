7caacd299313deafead40a24427d398dfd72ba26
ingenierd <bejo@posteo.de>
2023-02-28 21:27:21 +0100

    vor umbau

---
+++ src/GrundStruktur.h
@@ -0,0 +1,12 @@
+#ifndef SRC_GRUNDSTRUKTUR_H_
+#define SRC_GRUNDSTRUKTUR_H_
+
+#include <gtkmm/window.h>
+#include <gtkmm/grid.h>
+
+class GrundStruktur : public Gtk::Window
+{
+
+}
+
+#endif SRC_GRUNDSTRUKTUR_H_
